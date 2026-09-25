d852c67a9c5fd5220527e8d795040382c60508ff
Lei Vincent Gonzales <178282666+leivincentgonzales@users.noreply.github.com>
2026-09-25 18:20:24 +0800

    welcome to pos system

--- TFA2/app/Views/index.php
+++ TFA2/app/Views/index.php
@@ -1,6 +1,6 @@
 
         <div class="row">
             <div class="col col-md-12">
-                <h1>Welcome back, <?= $name; ?>!</h1>
+                <h1>Welcome to POS System</h1>
 
         </div>
